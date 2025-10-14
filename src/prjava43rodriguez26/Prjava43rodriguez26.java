74e3b1c395f93e2d758778d91c325992ac176c0d
Héctor Rodríguez <rodrigueziniestahector@fpllefia.com>
2025-10-14 17:01:00 +0200

    Versió 0.2-A del projecte prjava43rodriguez26. Branca00 creada i modificada.

--- src/prjava43rodriguez26/Prjava43rodriguez26.java
+++ src/prjava43rodriguez26/Prjava43rodriguez26.java
@@ -32,8 +32,8 @@ public class Prjava43rodriguez26 extends Frame
         // "this" Frame fires WindowEvent its registered WindowEvent listener 
         // "this" Frame adds "this" object as a WindowEvent listener 
  
-      setTitle("prjava43rodriguez26");  // "this" Frame sets title 
-      setSize(250, 100);        // "this" Frame sets initial size 
+      setTitle("nova finestra prjava43rodriguez26");  // "this" Frame sets title 
+      setSize(450, 100);        // "this" Frame sets initial size 
       setVisible(true);         // "this" Frame shows 
    } 
  
