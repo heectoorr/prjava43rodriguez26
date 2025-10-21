4228ccb75a3961c6c257ad0c150be1aede4f3a0d
Héctor Rodríguez <rodrigueziniestahector@fpllefia.com>
2025-10-21 16:09:18 +0200

    Versió 0.4-B del projecte prjava43rodriguez26. Branca main (o master) modificada.

--- src/prjava43rodriguez26/Prjava43rodriguez26.java
+++ src/prjava43rodriguez26/Prjava43rodriguez26.java
@@ -1,7 +1,8 @@
 package prjava43rodriguez26; 
 import java.awt.*;        // using AWT containers and components 
 import java.awt.event.*;  // using AWT events and listener interfaces 
- 
+import java.net.*;
+
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava43rodriguez26 extends Frame 
       implements ActionListener, WindowListener { 
